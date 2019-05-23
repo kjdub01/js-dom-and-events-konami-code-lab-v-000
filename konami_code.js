85024ee6af071a89a5c4d9b2989fce05e8a0f069
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 18:06:35 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,4 +4,6 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   
-  document.body.addEventListener('keydown', function(e) 
+  document.body.addEventListener('keydown', function(e) {
+    
+  }
