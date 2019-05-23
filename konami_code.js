161fd534c28ee1afe3bac654120360e6cc13e53b
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 18:05:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   
-  function onKeyDownHandler(e) {
+  document.body.addEventListener
     const key = parseInt(e.detail || e.which);
     
     if (key === code[index]) {
