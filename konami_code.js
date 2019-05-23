21c01e38ac952768043ed3271b1aa84635bb815a
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 18:06:29 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,19 +4,4 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
-    
-    if (key === code[index]) {
-      index++;
-      
-      if (index === code.length) {
-        alert("hurray!");
-        
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
-}
+  document.body.addEventListener('keydown', function(e) 
