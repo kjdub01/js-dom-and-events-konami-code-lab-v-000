d0bd707b9eb2987d24839ac3c3227f4311f062cb
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 18:08:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,6 +10,12 @@ function init() {
     if (key === code[index]) {
       index++;
       
-      
+      if (index === code.length) {
+        alert("Hurray!");
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
     }
   }
